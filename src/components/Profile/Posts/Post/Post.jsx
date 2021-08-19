d5cc9d2e7f73705adec8661c38e1d858ae28b552
Maksim Olf <olefirm83@gmail.com>
2021-08-19 22:30:25 +0300

    style

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import styles from './Post.module.css'
 
 
-const Post = () => {
+const Post = (props) => {
 	return (
 		<div class={styles.post}>
-			<div class={styles.text}>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem excepturi possimus molestias itaque alias. Reprehenderit veniam nisi omnis earum consectetur reiciendis pariatur vero eum repellat repudiandae voluptates vitae sapiente officia animi fugit in iusto culpa odit consequuntur consequatur, numquam accusantium.
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem excepturi possimus molestias itaque alias. Reprehenderit veniam nisi omnis earum consectetur reiciendis pariatur vero eum repellat repudiandae voluptates vitae sapiente officia animi fugit in iusto culpa odit consequuntur consequatur, numquam accusantium.
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem excepturi possimus molestias itaque alias. Reprehenderit veniam nisi omnis earum consectetur reiciendis pariatur vero eum repellat repudiandae voluptates vitae sapiente officia animi fugit in iusto culpa odit consequuntur consequatur, numquam accusantium.
+			<div className={styles.message}>
+				<div className={styles.text}>
+					{props.text}
+				</div>
+				<div className={styles.likes}>
+					<div className={styles.numbLikes}>
+						{props.nLikes}
+					</div>
+					<button className={styles.likeButton}>
+						<i class="far fa-heart"></i>
+						like
+					</button>
+				</div>
 			</div>
-			<img class={styles.image} src="https://cs-site.ru/uploads/posts/2020-09/1600253903_35.jpg" alt="Avatar not found" />
+			<div className={styles.person}>
+				<img className={styles.image} src={props.src} alt="Avatar not found" />
+				<div className={styles.name}>
+					{props.name}
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -10,13 +10,9 @@ const Posts = () => {
 				<textarea required className={styles.text} placeholder="Type your message here..."></textarea>
 				<button type="submit" className={styles.button}>Send</button>
 			</form>
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
-			<Post />
+			<Post nLikes="6" name="Rocket Fdasdasd" text="Nds kmk mkllewkm  kwem kewm kwem wem ekw!!!" src="https://cs-site.ru/uploads/posts/2020-09/1600253903_35.jpg" />
+			<Post nLikes="15" name="Groot" text="I AM GROOT!!!" src="https://gamehag.com/static/avatar/5194053_max.jpg" />
+
 		</div>
 	)
 }
